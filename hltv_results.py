31a51e5e08d4cd800661c479b9cce1f355e7482d
caojiachen1 <caojiachen1@163.com>
2022-09-06 11:50:30 +0800

    -a

--- hltv_results.py
+++ hltv_results.py
@@ -1,16 +1,18 @@
-from msilib.schema import ComboBox, ListBox
 import requests
 from bs4 import BeautifulSoup as bs
 import re
 import tkinter
 import tkinter.ttk
+import tkinter.messagebox as msgbox
 
 def get():
-    global teams_lose , teams_win , scores_lose , scores_win , event , info_list , info_dict , current_selection
+    global teams_lose , teams_win , scores_lose , scores_win , event , info_list , info_dict , link_list , get_link , j
     teams_lose , teams_win , scores_lose , scores_win = [] , [] , [], []
     event = []
     info_list = []
+    link_list = []
     info_dict = {}
+    get_link = {}
     url = r'https://www.hltv.org/results'
     s = bs(requests.get(url).text , 'html.parser')
     for i in s.find_all('div' , class_ = 'team'):
@@ -39,6 +41,11 @@ def get():
         if r is not None:
             scores_win.append(str(r.group(1)))
 
+    for i in s.find_all('a'):
+        j = str(i.get('href'))
+        if i.get('class') == ['a-reset'] and j[:8] == '/matches' and (j not in link_list):
+            link_list.append('https://www.hltv.org' + j)
+
     num = teams_lose.__len__()
     info_dict['All'] = []
     for i in range(num):
@@ -51,11 +58,63 @@ def get():
             info_dict[event[i]].append(vs)
         if vs not in info_dict['All']:
             info_dict['All'].append(vs)
+        get_link[vs] = link_list[i]
 
     content.set(tuple(info_dict['All']))
 
 def comb_select(event):
     content.set(tuple(info_dict[str(comb.get())]))
+
+def get_details():
+    global url , n , a , s , map , r , team1_score , team2_score , scores , team1 , team2 , teams , content_ , info_string
+
+    map , team1_score , team2_score , scores , teams = [] , [] , [] , [] , []
+    team1 , team2 = '' , ''
+    info_string = ''
+
+    try:
+        n = results.get(results.curselection())
+    except:
+        return
+    
+    url = get_link[str(n)]
+    a = requests.get(url)
+    s = bs(a.text , 'html.parser')
+
+    for i in s.find_all('div' , class_ = 'mapname'):
+        i = str(i)
+        r = re.search(r'<div class="mapname">(.*?)</div>' , i , re.IGNORECASE)
+        if r is not None:
+            map.append(str(r.group(1)))
+    for i in s.find_all('div' , class_ = 'results-team-score'):
+        i = str(i)
+        r = re.search(r'<div class="results-team-score">(.*?)</div>' , i , re.IGNORECASE)
+        if r is not None:
+            r = str(r.group(1))
+            scores.append(r)
+        
+    for i in range(scores.__len__()):
+        if (i % 2) ==0:
+            team1_score.append(scores[i])
+        else:
+            team2_score.append(scores[i])
+
+    for i in s.find_all('div' , class_ = 'teamName'):
+        i = str(i)
+        r = re.search(r'<div class="teamName">(.*?)</div>' , i , re.IGNORECASE)
+        if r is not None:
+            teams.append(str(r.group(1)))
+    
+    team1 , team2 = teams[0] , teams[1]
+
+    scores = []
+    for i in range(team1_score.__len__()):
+        scores.append('Map{}:{}\n'.format(i + 1 , map[i]) + team1.ljust(25) + team1_score[i].ljust(2) + '-' + team2_score[i].rjust(2) + team2.rjust(25))
+    for i in scores:
+        info_string = info_string + i + '\n'
+
+    msgbox.showinfo('详情' , info_string)
+    
 
 root = tkinter.Tk()
 root.title('CSGO职业比赛结果查询')
@@ -70,11 +129,14 @@ get()
 comb = tkinter.ttk.Combobox(root , width = 55)
 comb['value'] = tuple(info_dict.keys())
 comb.current(0)
-comb.place(relx = 0.2 , rely = 0.04)
+comb.place(relx = 0.15 , rely = 0.04)
 comb.bind('<<ComboboxSelected>>' , comb_select)
 
 refresh = tkinter.Button(root , text = '刷新' , command = get , width = 5)
-refresh.place(relx = 0.75 , rely = 0.035)
+refresh.place(relx = 0.7 , rely = 0.035)
+
+detail = tkinter.Button(root , text = '详情' , command = get_details , width = 5)
+detail.place(relx = 0.8 , rely = 0.035)
 
 y = tkinter.Scrollbar(root , width = 20)
 y.pack(side = tkinter.RIGHT , fill = tkinter.Y)
