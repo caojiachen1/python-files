bceed29652c4980e1e44b12bbedc841c1a43aa4d
caojiachen1 <caojiachen1@163.com>
2022-09-12 23:44:35 +0800

    -a

--- 叶悠悠电脑版_gui.py
+++ 叶悠悠电脑版_gui.py
@@ -7,6 +7,7 @@ import re
 import tkinter.messagebox as msgbox
 #需要手机打开叶悠悠聊天界面，程序自动将输入内容录入聊天框发送，并获取叶悠悠的回复
 #如果没有打开聊天界面或者中途退出聊天界面，程序将直接结束，需要重新启动
+#聊天框回车发送消息，按下Esc键退出程序
 
 def enter(event):
     a = os.popen('adb shell dumpsys window | findstr mCurrentFocus')
@@ -18,22 +19,22 @@ def enter(event):
     send = entry.get()
     mine.set('我发的消息: {}'.format(send))
     d.send_keys(send)
-    d(resourceId="com.baidu.input:id/send_btn").click()
+    d(resourceId = "com.baidu.input:id/send_btn").click()
     d.set_fastinput_ime(False)
     time.sleep(4)
-    respond_text = d(resourceId="com.baidu.input:id/content")[-1].info['text']
+    respond_text = d(resourceId = "com.baidu.input:id/content")[-1].info['text']
     respond.set('叶悠悠回复: {}'.format(respond_text))
     entry.delete(0 , tkinter.END)
 
-def center_window(root : tkinter.Tk, width, height):
+def center_window(root : tkinter.Tk , width , height):
     screenwidth , screenheight = root.winfo_screenwidth() , root.winfo_screenheight()
-    size = '%dx%d+%d+%d' % (width , height , (screenwidth - width)/2 , (screenheight - height)/2)
+    size = '%dx%d+%d+%d' % (width , height , (screenwidth - width) / 2 , (screenheight - height) / 2)
     root.geometry(size)
 
 os.system('adb devices')
 try:
     d = u2.connect()
-    d(resourceId="com.baidu.input:id/input_ed").click()
+    d(resourceId = "com.baidu.input:id/input_ed").click()
 except:
     msgbox.showerror('错误' , '手机未连接!')
     exit()
