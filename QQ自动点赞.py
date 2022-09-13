a8d9ae64bbbd11cbe903d9df34fdc1bcbc4b4689
caojiachen1 <caojiachen1@163.com>
2022-09-13 16:34:48 +0800

    -a

--- QQ自动点赞.py
+++ QQ自动点赞.py
@@ -4,7 +4,7 @@ d = u2.connect()
 d.app_start('com.tencent.mobileqq')
 d.xpath('//*[@resource-id="android:id/tabs"]/android.widget.FrameLayout[2]/android.widget.RelativeLayout[1]/android.widget.TextView[1]').click()
 while True:
-    if d(resourceId="com.tencent.mobileqq:id/c7p" , selected = False).exists():
+    if d(resourceId="com.tencent.mobileqq:id/c7p" , selected = False).exists:
         d(resourceId="com.tencent.mobileqq:id/c7p" , selected = False).click()
     d.swipe(540 , 1400 , 540 , 800)
     d.xpath('//*[@resource-id="android:id/tabs"]/android.widget.FrameLayout[2]/android.widget.RelativeLayout[1]/android.widget.TextView[1]').click()
